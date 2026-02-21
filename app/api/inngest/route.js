9c9be2230c5ab7f3a2806ee4b3a34bbccea19b04
Ayushi singh <ayushiisingh08@gmail.com>
2026-02-21 23:38:42 +0530

    Fix Inngest signing key config explicitly

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,14 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 
-// create an api that serves zero functions
+// create an api that serves inngest functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        /*your functions will be here later */
         syncUserCreation,
         syncUserUpdation,
         syncUserDeletion,
     ],
-});       
+    signingKey: process.env.INNGEST_SIGNING_KEY,
+    signingKeyFallback: process.env.INNGEST_SIGNING_KEY,
+});
